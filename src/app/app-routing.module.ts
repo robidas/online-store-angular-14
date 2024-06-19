246441390883d873a744032493176767cf6ca0b8
Rob Das <noreply@dev2>
2024-06-19 00:03:22 -0600

    navigation buttons in the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartPageComponent } from './start-page/start-page.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 export const routes: Routes = [
   { path: 'start', component: StartPageComponent },
   { path: 'stuff', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: '', redirectTo: 'start', pathMatch: 'full' }
 ];
 
